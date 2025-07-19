91483e6773abfde711d81fbd8d5f64f840124acc
HetPatel2111 <168984484+HetPatel2111@users.noreply.github.com>
2025-07-19 20:12:59 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (33.56%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,16 +1,21 @@
+/*In this problem we have odd even problem . single element than follow the the first occurence is is even pos and second occurence in odd position . exact opposite for the after single element*/
+
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        unordered_map<int,int> m;
+       int l=0;
+       int h=nums.size()-1;
 
-        for(int i=0 ; i<nums.size() ; i++){
-            m[nums[i]]++;
-        }
+       while(l<h){
+            int mid= l + (h-l)/2;
+            if (mid % 2 == 1) mid--;
 
-        for(auto it : m){
-            if(it.second==1) return it.first;
-        }
-
-        return 0;
+            if (nums[mid] == nums[mid + 1]) {
+                l = mid + 2;
+            } else {
+                h = mid;
+            }
+       }
+       return nums[h];
     }
 };
