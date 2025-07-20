d9e166443614f3ac056feaaf620582a05b816796
HetPatel2111 <168984484+HetPatel2111@users.noreply.github.com>
2025-07-20 18:37:23 +0530

    Time: 0 ms (100.00%) | Memory: 53 MB (89.43%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
@@ -1,17 +1,22 @@
+/*In this we search on the original array . in original array we get that value in those idx of the array we change sign . if sign is not there mean that is not in array*/
+
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        vector<int> ans(nums.size(),0);
-
+        vector<int> ans;
         for(int i=0 ; i<nums.size() ; i++){
-            ans[nums[i]-1]=-1;
+           if(nums[abs(nums[i]) - 1] > 0){
+                nums[abs(nums[i])-1] = (-1)*nums[abs(nums[i])-1];
+            }
         }
 
         for(int i=0 ; i<nums.size() ; i++){
-            if(ans[i]==0) ans[i]=i+1;
+            if(nums[i]>0){
+                ans.push_back(i+1);
+            }
         }
 
-        ans.erase(remove(ans.begin(), ans.end(), -1), ans.end());
+        //ans.erase(remove(ans.begin(), ans.end(), -1), ans.end());
 
         return ans;
     }
