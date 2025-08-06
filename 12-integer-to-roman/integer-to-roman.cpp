8630f962076b9fdc43042c301d3d223d2eef5f6c
HetPatel2111 <168984484+HetPatel2111@users.noreply.github.com>
2025-08-06 17:39:19 +0530

    Time: 12 ms (9.10%) | Memory: 16.2 MB (5.00%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        vector<int> roman = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        vector<string> ch ={"M","CM","D","CD","C","XC","L","XL","X","IX","V", "IV","I"};
-
-        string ans;
-
-        for(int i=0 ; i<roman.size() ; i++){
-            while(num>=roman[i]){
-                num -= roman[i];
-                ans+=ch[i];
-            }
+    multimap<int, string, greater<int>> roman; // Sort in descending order
+    roman.insert({1000, "M"});
+    roman.insert({900, "CM"});
+    roman.insert({500, "D"});
+    roman.insert({400, "CD"});
+    roman.insert({100, "C"});
+    roman.insert({90, "XC"});
+    roman.insert({50, "L"});
+    roman.insert({40, "XL"});
+    roman.insert({10, "X"});
+    roman.insert({9, "IX"});
+    roman.insert({5, "V"});
+    roman.insert({4, "IV"});
+    roman.insert({1, "I"});
+    
+    string result = "";
+    for (auto &p : roman) {
+        while (num >= p.first) {
+            num -= p.first;
+            result += p.second;
         }
-
-        return ans;
     }
+    return result;
+}
 };
