0e6f3b7b7494008ac59012694ece6c791ef2e97c
HetPatel2111 <168984484+HetPatel2111@users.noreply.github.com>
2025-08-27 18:43:46 +0530

    Time: 29 ms (95.30%) | Memory: 18.6 MB (53.04%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -9,20 +9,42 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        unordered_set<ListNode*> visited;
+        int len1=0;
+        int len2=0;
 
         ListNode* temp1 = headA;
-        ListNode* temp2 = headB;
-        while(temp1!=nullptr){
-            visited.insert(temp1);
+        ListNode*temp2 = headB;
+
+        while(temp1 != nullptr){
+            len1++;
             temp1 = temp1->next;
         }
 
         while(temp2 != nullptr){
-            if(visited.find(temp2) != visited.end()) return temp2;
+            len2++;
             temp2 = temp2->next;
         }
 
-        return nullptr;
+        bool BigFirstList = true;
+        if(len2 > len1) BigFirstList=false; 
+        int maxLen = max(len1,len2);
+        int minLen = min(len1,len2);
+        int shift = maxLen - minLen;
+        temp1=headA;
+        temp2=headB;
+        if(BigFirstList){
+            while(shift--) temp1 = temp1->next;
+        }
+        else {
+            while(shift--) temp2 = temp2->next;
+        }
+
+        while(temp1 != nullptr){
+            if(temp1 == temp2) return temp1;
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+        } 
+
+        return nullptr;   
     }
 };
