4302a86fa8873c14da3b6aee1fa715e936c74eff
HetPatel2111 <168984484+HetPatel2111@users.noreply.github.com>
2025-09-12 08:46:56 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (25.34%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> s1;
+
+        for(int i=0 ; i<s.size() ; i++){
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
+                s1.push(s[i]);
+            }
+            else  if(s1.empty() ||(s[i]==')' && s1.top()!='(') || (s[i]==']') && s1.top()!='[' ||  (s[i]=='}' && s1.top()!='{')) {
+                return false;
+            }
+            else{
+                s1.pop();
+            }
+        }
+
+        if(!s1.empty()) return false;
+        return true; 
+    }
+};
