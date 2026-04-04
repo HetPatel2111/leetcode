b21445cf8f5a2413ccbb4196b2de98dcb886c286
HetPatel2111 <168984484+HetPatel2111@users.noreply.github.com>
2026-04-04 20:48:25 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (6.48%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,21 +1,24 @@
-/*In this problem we have odd even problem . single element than follow the the first occurence is is even pos and second occurence in odd position . exact opposite for the after single element*/
-/*in this we fixed our mid that that is even and  and it right side is same mean (even,odd) pair which is happing on the left side mean we want to go right side*/
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-       int l=0;
-       int h=nums.size()-1;
+        int n=nums.size();
+        int l=0;
+        int h=n-1;
 
-       while(l<h){
-            int mid= l + (h-l)/2;
-            if (mid % 2 == 1) mid--;
+        while(l<h){
+            int mid = l + (h-l)/2;
 
-            if (nums[mid] == nums[mid + 1]) {
-                l = mid + 2;
-            } else {
-                h = mid;
+            if(mid%2==1) mid--;
+            int next = (mid+1)%n;
+
+            if(nums[mid]==nums[next]){
+                l=l+2;
             }
-       }
-       return nums[h];
+            else{
+                h=mid-1;
+            }
+        }
+
+        return nums[l];
     }
 };
