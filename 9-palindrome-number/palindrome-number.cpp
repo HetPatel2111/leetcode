8ecc46f7ee12de3d2767d3ec9df2ee35ee2f11b8
HetPatel2111 <168984484+HetPatel2111@users.noreply.github.com>
2026-04-10 20:10:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (37.54%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,9 +1,25 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        string s = to_string(x);
-        string rev = s;
-        reverse(rev.begin(), rev.end());
-        return s == rev;
+        if(x<0) return false;
+
+        int temp=x;
+        int count=0;
+        while(temp>0){
+            count++;
+            temp/=10;
+        }
+        
+        temp=x;
+        long long sum=0;
+        while(temp>0){
+            int mod = temp%10;
+            temp/=10;
+            count--;
+            sum+=(long long)mod*pow(10,count);
+        }
+
+        if(sum==x)return true;
+        return false;
     }
 };
