3365d7e899c4b6f6635ba27d1e7d8f540eb02cb7
HetPatel2111 <168984484+HetPatel2111@users.noreply.github.com>
2026-04-10 18:18:09 +0530

    Time: 3 ms (81.73%) | Memory: 14.3 MB (46.07%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<char> st;
+
+        for(auto &it : s){
+            if(!st.empty()){
+                if(st.top()==it){
+                    st.pop();
+                }
+                else{
+                    st.push(it);
+                }
+            }
+            else{
+                st.push(it);
+            }
+        }
+
+        string ans;
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+
+        reverse(ans.begin(),ans.end());
+
+        return ans;
+    }
+};
