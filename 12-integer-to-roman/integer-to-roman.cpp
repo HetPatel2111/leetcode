8027bf6b5ec881a60a5966ada5271d40ac4a6d4b
HetPatel2111 <168984484+HetPatel2111@users.noreply.github.com>
2026-04-13 16:07:46 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (81.23%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,28 +1,13 @@
 class Solution {
 public:
     string intToRoman(int num) {
-    multimap<int, string, greater<int>> roman; // Sort in descending order
-    roman.insert({1000, "M"});
-    roman.insert({900, "CM"});
-    roman.insert({500, "D"});
-    roman.insert({400, "CD"});
-    roman.insert({100, "C"});
-    roman.insert({90, "XC"});
-    roman.insert({50, "L"});
-    roman.insert({40, "XL"});
-    roman.insert({10, "X"});
-    roman.insert({9, "IX"});
-    roman.insert({5, "V"});
-    roman.insert({4, "IV"});
-    roman.insert({1, "I"});
-    
-    string result = "";
-    for (auto &p : roman) {
-        while (num >= p.first) {
-            num -= p.first;
-            result += p.second;
-        }
+        string ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+
+        string tens[]= {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+
+        string hurns[]={"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        string thous[]={"","M","MM","MMM"};
+
+        return (thous[num/1000] + hurns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10]);
     }
-    return result;
-}
 };
