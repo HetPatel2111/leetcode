cc07659f3258c41f6d48e338041e4c2f3c18f951
HetPatel2111 <168984484+HetPatel2111@users.noreply.github.com>
2026-04-14 08:40:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (60.64%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -2,15 +2,18 @@ class Solution {
 public:
     int maxDepth(string s) {
         int count=0;
-        int max1=0;
+        int maxCount=0;
 
-        for(int i=0 ; i<s.size() ; i++){
-            if(s[i] == '(') count++;
-            else if(s[i] == ')') count--;
-
-            max1 = max(max1,count);
+        for(auto &it : s){
+            if(it=='('){
+                count++;
+                maxCount = max(maxCount,count);
+            }
+            else if(it==')'){
+                count--;
+            }
         }
 
-        return max1;
+        return maxCount;
     }
 };
