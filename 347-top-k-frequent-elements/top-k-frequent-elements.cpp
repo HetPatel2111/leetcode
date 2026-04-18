12ab678b7446445c3e4a1e87e34201e51c178afb
HetPatel2111 <168984484+HetPatel2111@users.noreply.github.com>
2026-04-18 18:37:17 +0530

    Time: 1 ms (63.72%) | Memory: 18.2 MB (30.62%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        if(nums.size() == k) return nums;
         unordered_map<int,int> u;
         vector<int> ans;
         for(auto &it : nums) u[it]++;
 
-        multimap<int,int,greater<int>> m;
+        multimap<int,int,greater<int>> mm;
 
-        for(auto &it : u) m.insert({it.second,it.first});
+        for(auto &it : u) mm.insert({it.second,it.first});
 
-        for(auto &it : m){
+        for(auto &it : mm){
             ans.push_back(it.second);
             k--;
-
-            if(k==0) break;
+            if(k==0) return ans;
         }
 
         return ans;
