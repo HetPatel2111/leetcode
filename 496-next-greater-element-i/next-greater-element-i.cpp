464211411110dedfe8f6fb67a6377eae4ff9ce3d
HetPatel2111 <168984484+HetPatel2111@users.noreply.github.com>
2026-04-23 15:32:59 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (59.21%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,27 +1,40 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> next;
-        stack<int> s;
+        unordered_map<int,int> u;
+        vector<int> ans;
+        stack<int> st;
 
-        for (int i = nums2.size() - 1; i >= 0; --i) {
-            while (!s.empty() && s.top() <= nums2[i]) {
-                s.pop();
-            }
+        for(int i=nums2.size()-1 ; i>=0 ; i--){
+            if(!st.empty()){
+                if(nums2[i] < st.top()){
+                    u[nums2[i]] = st.top();
+                    st.push(nums2[i]);
+                    continue;
+                }
 
-            if(s.empty()){
-                next[nums2[i]] = -1;
+                while(!st.empty() && nums2[i]>=st.top()) st.pop();
+
+                if(st.empty()){
+                    st.push(nums2[i]);
+                    u[nums2[i]]=-1;
+                }
+                else{
+                    u[nums2[i]] = st.top();
+                    st.push(nums2[i]);
+                }
             }
             else{
-                next[nums2[i]] = s.top();
+                st.push(nums2[i]);
+                u[nums2[i]] = -1;
             }
-            s.push(nums2[i]);
         }
 
-        vector<int> res;
-        for (int num : nums1) {
-            res.push_back(next[num]);
+        for(auto &it : nums1){
+            ans.push_back(u[it]);
         }
-        return res;
+
+        return ans;
+
     }
 };
