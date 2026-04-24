efadd6fbe558e0c64569d3374ed0f935fcdf97f4
HetPatel2111 <168984484+HetPatel2111@users.noreply.github.com>
2026-04-24 16:08:56 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (5.73%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
+        int n=s.size();
+        stack<int> st;
 
-        for(int i=0 ; i<s.size() ; i++){
-            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
-                st.push(s[i]);
-                continue;
-            }
-
-            if(st.empty() && (s[i]==')' || s[i]=='}' || s[i]==']')) return false;
-
-            if(s[i]==')'&&st.top()!='(' || s[i]==']'&&st.top()!='[' || s[i]=='}'&&st.top()!='{') return false;
-
-            st.pop();
-            
+        for(auto &it : s){
+            if(it=='(' || it=='[' || it=='{') st.push(it);
+            else if(st.empty() && it==')' || st.empty() && it=='}' || st.empty() && it==']') return false;
+            else if(it==')' && st.top()!='(' || it==']' && st.top()!='[' || it=='}' && st.top()!='{') return false;
+            else st.pop();
         }
 
         if(!st.empty()) return false;
