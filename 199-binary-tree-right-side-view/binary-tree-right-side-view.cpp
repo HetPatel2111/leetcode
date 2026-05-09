17ebb88eadfea9c634de4e9100feb871ca588624
HetPatel2111 <168984484+HetPatel2111@users.noreply.github.com>
2026-05-09 21:29:29 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (95.62%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -9,23 +9,20 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
- /* using approach LRV (reverse of in order)*/
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        vector<int> ans;
         int level=0;
+        vector<int> ans;
         traversal(root,level,ans);
         return ans;
     }
 
-    void traversal(TreeNode* root , int level , vector<int>&ans){
+    void traversal(TreeNode*root , int level , vector<int>&ans){
         if(!root) return;
-
         if(level==ans.size()) ans.push_back(root->val);
 
-        traversal(root->right , level+1 , ans);
-        traversal(root->left , level+1 , ans);
+        traversal(root->right,level+1,ans);
+        traversal(root->left,level+1,ans);
     }
 };
