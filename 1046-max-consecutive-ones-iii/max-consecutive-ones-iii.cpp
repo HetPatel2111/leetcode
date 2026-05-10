256464e6bef774ea7203e04b24498914b060e307
HetPatel2111 <168984484+HetPatel2111@users.noreply.github.com>
2026-05-10 15:58:49 +0530

    Time: 0 ms (100.00%) | Memory: 69.5 MB (59.06%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -4,28 +4,27 @@ public:
         int maxi=0;
         int left=0;
         int right=0;
+        int n=nums.size();
         int zeros=0;
 
-        while(right < nums.size()){
+        while(right<n){
             if(nums[right]==0){
                 zeros++;
             }
 
-            /*Move left pointer to first zero next member*/
             while(zeros > k){
-                if(nums[left] == 0){
-                    zeros--;
-                }
+                if(nums[left]==0) zeros--;
                 left++;
             }
 
-            if(zeros <= k){
+            if(zeros<=k){
                 int len = right-left+1;
                 maxi = max(maxi,len);
             }
 
             right++;
         }
+
         return maxi;
     }
 };
