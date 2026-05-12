193581d6989231e2ea063963b82d0a2f57be8457
HetPatel2111 <168984484+HetPatel2111@users.noreply.github.com>
2026-05-12 16:32:15 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (27.79%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -4,13 +4,9 @@ public:
         int n=nums.size();
         int l=0;
         int h=n-1;
-
         while(l<=h){
-            int mid = l +(h-l)/2;
-
-            if(nums[mid]==target){
-                return true;
-            }
+            int mid = l + (h-l)/2;
+            if(nums[mid]==target) return true;
 
             if(nums[l]==nums[mid] && nums[mid]==nums[h]){
                 l++;
@@ -19,22 +15,14 @@ public:
             }
 
             if(nums[l]<=nums[mid]){
-                if(nums[l]<=target && target<=nums[mid]){
-                    h=mid-1;
-                }
-                else{{
-                    l=mid+1;
-                }}
+                if(nums[l]<=target && target<=nums[mid]) h=mid-1;
+                else l=mid+1;
             }
             else{
-                if(nums[mid]<=target && target<=nums[h]){
-                    l=mid+1;
-                }
-                else{{
-                    h=mid-1;
-                }}
+                if(nums[mid]<=target && target<=nums[h]) l=mid+1;
+                else h=mid-1;
             }
-        }
+        } 
         return false;
     }
 };
