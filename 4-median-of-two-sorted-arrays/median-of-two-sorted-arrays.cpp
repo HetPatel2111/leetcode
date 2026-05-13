24bde8fb002b7e6e57076be1e26755b60925f38e
HetPatel2111 <168984484+HetPatel2111@users.noreply.github.com>
2026-05-13 11:03:26 +0530

    Time: 7 ms (9.19%) | Memory: 96.1 MB (8.75%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,42 +1,40 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n=nums1.size();
-        int m=nums2.size();
+        int m=nums1.size();
+        int n=nums2.size();
+
+        vector<int> ans;
+
         int i=0;
         int j=0;
-        vector<int> merge;
 
-        while(i<n && j<m){
-            if(nums1[i] > nums2[j]){
-                merge.push_back(nums2[j]);
-                j++;
+        while(i<m && j<n){
+            if(nums1[i] < nums2[j]){
+                ans.push_back(nums1[i]);
+                i++;
             }
             else{
-                merge.push_back(nums1[i]);
-                i++;
+                ans.push_back(nums2[j]);
+                j++;
             }
         }
 
-        while(i<n){
-            merge.push_back(nums1[i]);
+        while(i<m){
+            ans.push_back(nums1[i]);
             i++;
         }
 
-        while(j<m){
-            merge.push_back(nums2[j]);
+        while(j<n){
+            ans.push_back(nums2[j]);
             j++;
         }
 
-        int o = merge.size();
+        if(ans.size()%2) return ans[ans.size()/2];
 
-        if(o%2==1){
-            return merge[o/2];
-        }
-        else{
-            double x = merge[o/2];
-            double y = merge[(o-1)/2];
-            return (x+y)/2;
-        }
+        double x = ans[ans.size()/2-1];
+        double y = ans[ans.size()/2];
+
+        return (x+y)/2;
     }
 };
