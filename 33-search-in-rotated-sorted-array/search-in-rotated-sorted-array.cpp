b1c944df953dd63a10e53341cf273cf3329724f2
HetPatel2111 <168984484+HetPatel2111@users.noreply.github.com>
2026-05-22 13:13:57 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (66.43%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -4,8 +4,10 @@ public:
         int n=nums.size();
         int l=0;
         int h=n-1;
+
         while(l<=h){
             int mid = l + (h-l)/2;
+
             if(nums[mid]==target) return mid;
 
             if(nums[l]<=nums[mid]){
@@ -13,10 +15,11 @@ public:
                 else l=mid+1;
             }
             else{
-                if(nums[mid]<=target && target<=nums[h]) l=mid+1;
+                if(nums[mid]<=target && target<=nums[h])l=mid+1;
                 else h=mid-1;
             }
-        } 
+        }
+
         return -1;
     }
 };
