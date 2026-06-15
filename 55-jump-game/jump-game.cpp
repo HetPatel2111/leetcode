f4f0d8ae744ee8bc2f70e1f954706564f1f9aeaf
HetPatel2111 <168984484+HetPatel2111@users.noreply.github.com>
2026-06-15 20:55:45 +0530

    Time: 0 ms (100.00%) | Memory: 52.4 MB (57.61%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int last = nums.size()-1;
+        int last=nums.size()-1;
         int maxIdx=0;
-        
-        // in every step we calculate how far we reach from current idx
-        // if current idx is less than maxIdx mean we can't reach here
+
         for(int i=0 ; i<nums.size() ; i++){
             if(maxIdx < i) return false;
             if(maxIdx >= last) return true;
-            maxIdx = max(maxIdx , nums[i]+i);
+            maxIdx = max(maxIdx,nums[i]+i);
         }
+
         return true;
     }
 };
