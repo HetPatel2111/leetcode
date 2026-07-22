81a0a20b01ea0a201c791a10b7d1d1ea7d83e518
HetPatel2111 <168984484+HetPatel2111@users.noreply.github.com>
2026-07-22 07:59:29 +0530

    Time: 6 ms (66.23%) | Memory: 11.8 MB (55.58%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map<char,int> u;
         int n=s.size();
-        int left=0;
+        unordered_map<char,int> u;
         int ans=0;
+        int left=0;
 
         for(int i=0 ; i<n ; i++){
-            if(u.find(s[i])==u.end()){
-                u.insert({s[i],i});
+            char ch=s[i];
+            if(u.find(ch)==u.end()){
+                u.insert({ch,i});
                 ans = max(ans,i-left+1);
                 continue;
             }
 
-            left = max(left,u[s[i]]+1);
-            ans = max(ans,i-left+1);
-            u[s[i]]=i;
-            
-            
+            left = max(left,u[ch]+1);
+            u[ch]=i;
+            ans= max(ans,i-left+1);
         }
 
         return ans;
